6ce40cfc613987e05774a186827cae3e9363385d
Ulrike Schnoor <ulrike.schnoor@cern.ch>
2018-06-16 19:12:14 +0200

    start to optimize the optimizer

--- Optimizer/Optimizer.py
+++ Optimizer/Optimizer.py
@@ -19,72 +19,41 @@ def func_Surplus_day(t):
 
     #t is starting time: the index of the bin where it starts to be a certain value
     #array i
-    print((t[0]))
-    before_t0 = np.zeros(int(t[0]))
-    during_appliance = np.array([0.5,0.5,0.5])
-    after_t0plus3 = np.zeros(24-int(t[0])-3)
-    #print(type(before_t0), type(during_appliance), type(after_t0plus3))
-    #print(before_t0, during_appliance, after_t0plus3)
-    
-    o_1 = np.append(before_t0,during_appliance) #,after_t0plus3)
-    optimizable_appliance = np.append( o_1 ,after_t0plus3)
-    #print(t[0])
-    #print(optimizable_appliance)
-    
-    #surplus = energyproduction - energyconsumption - optimizable_appliance
+    #print((t[0]))
+    optimizable_appliance = np.zeros(24)
+
+    optimizable_appliance[t:t+3] = 0.5
+
     surplus = energyproduction - energyconsumption- optimizable_appliance
-    #print(abs(surplus))
-    print(sum(abs(surplus)))
     
     return sum(abs(surplus))
-time_guess = np.array([3])
+
+#since we know that the optimal hour will be one of 24, can just loop over them without needing complex optimization
+
+times = np.arange(24-2)
+
+surplus = 9999
+index = -9
+
+for t in times:
+
+    surplus_t = func_Surplus_day(t)
+    print(t,surplus_t)
+    if abs(surplus_t) < surplus:
+        surplus = surplus_t
+        index = t
+
+indexlist =[]
+for t in times:
+    surplus_t = func_Surplus_day(t)
+    if abs(surplus_t) == surplus:
+        indexlist.append(t)
+
+
+print("Minimum surplus = {} for starting the appliance between {}h and {}h ".format(surplus,indexlist[0], indexlist[len(indexlist)-1]))
+
+#time_guess = np.array([1])
 
 #res = optimize.minimize(func_Surplus_day, time_guess, method='nelder-mead',  options={'xtol': 1e-8, 'disp': True})
-res = optimize.minimize(func_Surplus_day, time_guess, method='nelder-mead',  options={'xtol': 0.5, 'disp': True})
-print(res.x)
+#print(res.x)
 
-
-#
-#
-#
-##time
-#time = np.linspace(0, 24, 24)   # start, end, num-points
-##actually, this should be done in datetime
-#
-#
-#
-#
-#
-#
-#
-##prediction of energy input
-#def f_energyproduction(t):
-#    return  np.array([ - 3*(t[0]-4)**2 +20])
-#energyproduction = Energy("production", f_energyproduction) #(time))
-##print(f_energyproduction)
-##plt.plot(time, f_energyproduction, 'o')  # dot plot
-#
-##fixed energy consumption
-#def f_energyconsumption(t):
-#    return np.array([ t[0]**2- t[0]**4])
-#energyconsumption = Energy("consumption_fixed", f_energyconsumption) #(time))
-#
-##appliance_1 = Energy("washing_machine")
-##def wash(t):
-##    return t
-##appliance.setTimedependence()
-#
-#surplus = energyproduction.energy(time) - energyconsumption.energy(time)
-#E_surplus = Energy("surplus", surplus)
-#
-#print(surplus)
-#
-##def appliances(t):
-##    return np.array(  [   ]  )
-#
-#time = np.linspace(0, 24, 24*6)   # start, end, num-points
-#
-##res = optimize.minimize(energyconsumption, time)
-##res = optimize.minimize(energyproduction.energy, time)
-#res = optimize.minimize(surplus, time)
-#print(res)
